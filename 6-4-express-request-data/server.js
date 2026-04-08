1ceb27a4b00e0fd6d4f85941274c5fb0e0f5f00e
seel88 <s202270080@kfupm.edu.sa>
2026-04-08 08:24:01 +0300

    TODO 3: Implement GET /profile/:first/:last using req.params

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -132,7 +132,14 @@ app.get("/echo", (req, res) => {
 });
 
 // Route params: /profile/First/Last
-
+app.get("/profile/:first/:last", (req, res) => {
+    const { first, last } = req.params;
+    
+    res.json({ 
+        ok: true, 
+        fullName: `${first} ${last}` 
+    });
+});
 
 // Route param middleware example: /users/42
 
